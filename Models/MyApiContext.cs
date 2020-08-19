c217b7ed57e6ed8841d1930034db9e14f141b25b
Mbaye Sokhna THIAM <mbayesokhnathiam@gmail.com>
2020-08-19 01:12:06 +0000

    ASP.Net Core + Angular Backend OK

---
+++ Models/MyApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAngular.Models
+{
+    public class MyApiContext:DbContext
+    {
+        public MyApiContext(DbContextOptions<MyApiContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Custumer> Custumers { get; set; }
+    }
+}
